53075867d9927b6982389cd8cc25440932d0d769
Safiy-Hagamed <safiyhagamed@gmail.com>
2024-09-07 19:20:06 +0530

    made changes in passwords

--- testing/passwords.py
+++ testing/passwords.py
@@ -1,14 +1,18 @@
+''' This code is for a studying and dont take this code seriously and store your important passwords in this code. '''
 user = input ("enter the main password: ")
 
 def view():
-	print("Hello worlds")
-
-
+	with open('passwords.txt', 'r') as f:
+		for line in f.readlines():
+			data = line.rstrip()
+			user, passw = data.split("|")
+			print ("user: ", user, ",", "password: ", passw)
 def add():
 	name = input ("Account Name: ")
 	passwd = input ("Password: ")
-	with open("passwords.txt", 'a') as file:
-		file.write (name+ " | " + passwd)
+
+	with open('passwords.txt', 'a') as file:
+		file.write (name+ "|" + passwd + "\n")
 
 while True:
 	choice = input ("would you like to [ view/add ] or q to quit - ").lower()
